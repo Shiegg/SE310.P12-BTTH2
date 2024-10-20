118ab52094c605b5c5b1beed93f49603bd095b70
Shieng <22521312@gm.uit.edu.vn>
2024-10-20 20:39:12 +0700

    Update

---
+++ MotorbikeStore/Models/SalesRecords.cs
@@ -0,0 +1,13 @@
+﻿namespace MotorbikeStore.Models
+{
+    public class SalesRecords
+    {
+        public int SaleId { get; set; }
+        public int MotorbikeId { get; set; }
+        public int QuantitySold { get; set; }
+        public DateTime SaleDate { get; set; }
+
+        // Thiết lập mối quan hệ với Motorbike
+        public Motorbike Motorbike { get; set; }
+    }
+}
